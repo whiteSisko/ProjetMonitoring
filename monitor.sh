0b64b4ef71a1f0b03411460ce6ffd2b1a23a9b47
whiteSisko <whitesisko@proton.me>
2025-12-11 12:20:06 +0000

    rajoue de l'erreur pour les tests

--- monitor.sh
+++ monitor.sh
@@ -8,7 +8,7 @@ function get_ram_usage {
 
 function get_cpu_usage {
     echo "--- Utilisation CPU ---"
-    CPU_IDLE=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
+    CPU_IDE=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
     echo "Utilisation actuelle du CPU: ${CPU_IDLE}%"
     echo ""
 }
